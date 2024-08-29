cebfd317f1cf427575207da17ebf0035c3376956
mardejesus <mardejesus001@gmail.com>
2024-08-29 16:20:38 -0300

    ReadFile class updated

--- formatter/src/main/kotlin/rules/RulesReader.kt
+++ formatter/src/main/kotlin/rules/RulesReader.kt
@@ -2,11 +2,17 @@ package rules
 
 import org.gradle.internal.impldep.org.yaml.snakeyaml.Yaml
 import java.io.File
-import java.io.FileInputStream
 
 class RulesReader {
-    fun readFile(path: String) : Map<String, Any>{
-        val yaml = File(path).readText(); // transforma el contenido a String
-        return Yaml().load(yaml) // convierte contenido a Map<String, Any>
+    fun readFile(path: String) : Map<String, Any> {
+        val yaml: String = File(path).readText(); // transforma el contenido a String
+        val rulesMap: Map<String, Any> = Yaml().load(yaml); // convierte contenido a Map<String, Any>
+
+        // chequea que se encuentren las configuraciones requeridas
+        val requiredRules = listOf("spaceBeforeColon", "spaceAfterColon", "spaceAroundEquals", "lineFeed");
+        val missingRules = requiredRules.filter { !rulesMap.containsKey(it) };
+        if(missingRules.isNotEmpty()) throw IllegalArgumentException("No se encuentran las siguientes reglas en el archivo: ${missingRules.joinToString(", ")}");
+
+        return rulesMap;
     }
 }
