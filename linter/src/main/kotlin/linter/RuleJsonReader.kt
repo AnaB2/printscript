2edce7faa36191cf5325469cc06f3e903cd5b5de
ValentinoGianuzzo <valentino.gianuzzo@ing.austral.edu.ar>
2024-10-16 22:35:59 -0300

    fix formatter cli

--- linter/src/main/kotlin/linter/RuleJsonReader.kt
+++ linter/src/main/kotlin/linter/RuleJsonReader.kt
@@ -1,7 +1,6 @@
 package linter
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import rules.CamelCaseRule
 import rules.InputOnlyRule
@@ -13,19 +12,17 @@ import java.io.File
 // Definición de reglas de formato
 class FormattingRules {
     @JsonProperty("identifier_format")
-    val identifier: String? = null
+    var identifier: String? = null
 
     @JsonProperty("enable_print_only")
-    val isEnablePrintOnly: Boolean = false
+    var isEnablePrintOnly: Boolean = false
 
     @JsonProperty("enable_input_only")
-    val isEnableInputOnly: Boolean = false
+    var isEnableInputOnly: Boolean = false
 }
 
 // Clase para leer y obtener reglas desde JSON
 class RuleJsonReader {
-    private val mapper: ObjectMapper = jacksonObjectMapper()
-
     // Método para leer reglas desde un archivo JSON
     fun getRulesFromFile(path: String): List<Rule> {
         val file = File(path)
@@ -34,27 +31,23 @@ class RuleJsonReader {
 
     // Método para leer reglas desde un contenido JSON en formato String
     fun getRulesFromJson(jsonContent: String): List<Rule> {
-        return try {
-            val formattingRules = mapper.readValue(jsonContent, FormattingRules::class.java)
-            val rules = mutableListOf<Rule>()
+        val mapper = jacksonObjectMapper()
+        val formattingRules = mapper.readValue(jsonContent, FormattingRules::class.java)
+        val rules = mutableListOf<Rule>()
 
-            // Normalizar y recortar el identificador para manejar variaciones como "camel case" o "snake case"
-            when (formattingRules.identifier?.replace(" ", "")?.lowercase()) {
-                "camelcase" -> rules.add(CamelCaseRule())
-                "snakecase" -> rules.add(SnakeCaseRule())
-            }
-
-            if (formattingRules.isEnablePrintOnly) {
-                rules.add(PrintOnlyRule())
-            }
-            if (formattingRules.isEnableInputOnly) {
-                rules.add(InputOnlyRule())
-            }
-
-            rules
-        } catch (e: Exception) {
-            println("Error al analizar el JSON: ${e.message}")
-            emptyList() // Retorna una lista vacía o puedes lanzar una excepción según tus necesidades
+        // Normalize and trim the identifier to handle variations like "camel case" or "snake case"
+        when (formattingRules.identifier?.replace(" ", "")?.lowercase()) {
+            "camelcase" -> rules.add(CamelCaseRule())
+            "snakecase" -> rules.add(SnakeCaseRule())
         }
+
+        if (formattingRules.isEnablePrintOnly) {
+            rules.add(PrintOnlyRule())
+        }
+        if (formattingRules.isEnableInputOnly) {
+            rules.add(InputOnlyRule())
+        }
+
+        return rules
     }
 }
